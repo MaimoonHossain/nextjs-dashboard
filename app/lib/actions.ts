a76ce1c7618207279c02532dda43361d23406cda
MaimoonHossain <ashikasitosh740@gmail.com>
2025-07-09 12:10:15 +0600

    chapter 13 compoleted

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,13 +74,25 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // ...
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -92,6 +104,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     console.error("Error updating invoice:", error);
   }
+
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
